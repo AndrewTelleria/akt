223ca5c798f27a9d33a74c18b3caa86cf0e89970
Andrew K Telleria <andrewktelleria@gmail.com>
2018-07-16 11:23:28 -0700

    home and blog model

--- blog/models.py
+++ blog/models.py
@@ -1,75 +1,191 @@
 from django.db import models
 
-from modelcluster.fields import ParentalKey
+from django import forms
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.db import models
+
+from modelcluster.fields import ParentalKey, ParentalManyToManyField
+from modelcluster.contrib.taggit import ClusterTaggableManager
+from taggit.models import TaggedItemBase
 
 from wagtail.core.models import Page, Orderable
 from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core import blocks
 from wagtail.admin.edit_handlers import (
-	FieldPanel,
-	FieldRowPanel,
-	InlinePanel,
-	MultiFieldPanel,
-	PageChooserPanel,
-	StreamFieldPanel,
+    FieldPanel, 
+    InlinePanel, 
+    MultiFieldPanel, 
+    StreamFieldPanel,
 )
+
+from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.edit_handlers import ImageChooserPanel
 from wagtail.search import index
-
-from home.blocks import BaseStreamBlock
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.edit_handlers import SnippetChooserPanel
 
 
 class BlogIndexPage(Page):
-	intro = RichTextField(blank=True)
+    intro = RichTextField(blank=True)
 
-	def get_context(self, request):
-		context = super().get_context(request)
-		blogpages = self.get_children().live().order_by('-first_published_at')
-		context['blogpages'] = blogpages
-		return context
+    class Meta:
+    	verbose_name = "blog indexpage"
+    content_panels = Page.content_panels + [
+        FieldPanel('intro', classname="full")
+    ]
+    subpage_types = ['Blogpage']
 
-	content_panels = Page.content_panels + [
-		FieldPanel('intro', classname="full"),
-	]
+
+    def get_context(self, request):
+        context = super(BlogIndexPage, self).get_context(request)
+        # Get the full unpaginated listing of resource pages as a queryset
+        all_resources = BlogPage.objects.descendant_of(
+            self).live().order_by(
+            '-first_published_at')
+        paginator = Paginator(all_resources, 5)
+        page = request.GET.get('page')
+        try:            
+            resources = paginator.page(page)
+        except PageNotAnInteger:
+            # If page is not an integer, deliver first page.
+            resources = paginator.page(1)
+        except EmptyPage:
+            # If page is out of range (e.g. 9999), deliver last page of results.
+            resources = paginator.page(paginator.num_pages)
+        context['resources'] = resources
+        return context
+
+
+class BlogPageTag(TaggedItemBase):
+
+    class Meta:
+        verbose_name = "blogpage tag"
+
+    content_object = ParentalKey(
+            'BlogPage',
+            related_name='tagged_items',
+            on_delete=models.CASCADE
+        )
 
 
 class BlogPage(Page):
-	date = models.DateField("Post date")
-	intro = models.CharField(max_length=255)
-	body = StreamField(
-		BaseStreamBlock(), verbose_name="Blog body", blank=True
-	)
+    author = models.ForeignKey(
+        'home.People',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+    )
+    FEATURE_1 = 1
+    FEATURE_2 = 2
+    FEATURE_3 = 3
+    FEATURE_4 = 4
+    FEATURE_5 = 5
+    FEATURE_6 = 6
+    NO_FEATURE = 0
+    FEATURE_CHOICES = (
+        (FEATURE_1, 'Feature 1'),
+        (FEATURE_2, 'Feature 2'),
+        (FEATURE_3, 'Feature 3'),
+        (FEATURE_4, 'Feature 4'),
+        (FEATURE_5, 'Feature 5'),
+        (FEATURE_6, 'Feature 6'),
+        (NO_FEATURE, 'No feature')
+    )
+    feature = models.IntegerField(
+        choices=FEATURE_CHOICES,
+        default=0,
+    )
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = StreamField([
+            ('heading', blocks.CharBlock(classname="full title")),
+            ('paragraph', blocks.RichTextBlock()),
+            ('image', ImageChooserBlock()),
+        ])
+    image = models.ForeignKey(
+        'wagtailimages.Image',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name='+',
+        help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
+    )
+    tags = ClusterTaggableManager(through=BlogPageTag, blank=True)
+    categories = ParentalManyToManyField('blog.BlogCategory', blank=True)
 
-	def main_image(self):
-		gallery_item = self.gallery_images.first()
-		if gallery_item:
-			return gallery_item.image
-		else:
-			return None
+    class Meta:
+        verbose_name = "blogpage"
 
-	search_fields = Page.content_panels + [
-		index.SearchField('intro'),
-		index.SearchField('body'),
-	]
+    search_fields = Page.search_fields + [
+        index.SearchField('author'),
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
 
-	content_panels = Page.content_panels + [
-		FieldPanel('date'),
-		FieldPanel('intro'),
-		StreamFieldPanel('body'),
-		InlinePanel('gallery_images', label="Gallery images"),
-	]
+    content_panels =  [
+        FieldPanel('title', classname="full"),
+        SnippetChooserPanel('author'),
+        FieldPanel('feature'),
+        MultiFieldPanel([
+            FieldPanel('date'),
+            FieldPanel('tags'),
+            FieldPanel('categories', widget=forms.CheckboxSelectMultiple),
+        ], heading="Blog information"),
+        FieldPanel('intro', classname="full"),
+        ImageChooserPanel('image'),
+        InlinePanel('gallery_images', label="Gallery images"),
+        StreamFieldPanel('body'),
+    ]
 
 
 class BlogPageGalleryImage(Orderable):
-	page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
-	image = models.ForeignKey(
-		'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
-	)
-	caption = models.CharField(blank=True, max_length=250)
+    page = ParentalKey(BlogPage, on_delete=models.CASCADE, related_name='gallery_images')
+    image = models.ForeignKey(
+            'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+        )
+    caption = models.CharField(blank=True, max_length=250)
+
+    panels = [
+        ImageChooserPanel('image'),
+        FieldPanel('caption'),
+    ]
+
+
+class BlogTagIndexPage(Page):
+
+	class Meta:
+		verbose_name = "blogtag indexpage"
+
+	def get_context(self, request):
+
+		# Filter by tag
+		tag = request.GET.get('tag')
+		blogpages = BlogPage.objects.filter(tags__name=tag)
+
+		# Update template context
+		context = super().get_context(request)
+		context['blogpages'] = blogpages
+		return context
+
+
+
+@register_snippet
+class BlogCategory(models.Model):
+	name = models.CharField(max_length=255)
+	icon = models.ForeignKey(
+			'wagtailimages.Image', null=True, blank=True,
+			on_delete=models.SET_NULL, related_name='+'
+		)
 
 	panels = [
-		ImageChooserPanel('image'),
-		FieldPanel('caption'),
-	]	
+		FieldPanel('name'),
+		ImageChooserPanel('icon'),
+	]
+
+	def __str__(self):
+		return self.name
+
 
 
 
--- home/models.py
+++ home/models.py
@@ -1,24 +1,405 @@
+from django import forms
 from django.db import models
+from django.core.validators import RegexValidator
 
-from wagtail.core.models import Page
+from modelcluster.fields import ParentalKey
+from modelcluster.models import ClusterableModel
+
 from wagtail.admin.edit_handlers import (
-	FieldPanel,
-	FieldRowPanel,
-	InlinePanel,
-	MultiFieldPanel,
-	PageChooserPanel,
-	StreamFieldPanel,
-)
+		FieldPanel,
+		FieldRowPanel,
+		InlinePanel,
+		MultiFieldPanel,
+		PageChooserPanel,
+		StreamFieldPanel,
+	)
+
 from wagtail.core.fields import RichTextField, StreamField
+from wagtail.core.models import Collection, Page, Orderable
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.search import index
+from wagtail.snippets.models import register_snippet
 
 from .blocks import BaseStreamBlock
 
+# from projects.models import ProjectPage
+from blog.models import BlogPage, BlogPageGalleryImage
+# from photo_gallery.models import PhotoGalleryPage, PhotoGalleryImage
+
+
+
+@register_snippet
+class People(ClusterableModel):
+	"""
+	A Django model to store people objects. It uses the '@register_snippet'
+	decorator to allow it to be accesible via the Snippets UI
+
+	'People' uses the 'ClusterableModel', which allows the relationship with
+	another model to be stored locally to the 'parent' model (e.g. a PageModel)
+	until the parent is explicitly saved. This allows the editor to use the
+	'Preview' button, to preview the content, without saving the relationships
+	to the database.
+	https://github.com/wagtail/django-modelcluster
+	"""
+	first_name = models.CharField("First name", max_length=254)
+	last_name = models.CharField("Last name", max_length=254)
+	job_title = models.CharField("Job title", max_length=254)
+	about = models.TextField(max_length=300)
+
+	image = models.ForeignKey(
+			'wagtailimages.Image',
+			null=True,
+			blank=True,
+			on_delete=models.SET_NULL,
+			related_name='+',
+		)
+
+	panels = [
+		FieldPanel('first_name', classname="first-name"),
+		FieldPanel('last_name', classname="last-name"),
+		FieldPanel('job_title'),
+		FieldPanel('about', classname="full"),
+		ImageChooserPanel('image'),
+	]
+
+	search_fields = Page.search_fields + [
+		index.SearchField('first_name'),
+		index.SearchField('last_name'),
+	]
+
+	@property
+	def thumb_image(self):
+		# Returns an empty string if there is no profile pic or the rendition
+		# file can't be found.
+		try:
+			return self.image.get_rendition('fill-50x50').img_tag()
+		except:
+			return ''
+
+	def __str__(self):
+		return '{} {}'.format(self.first_name, self.last_name)
+
+	class Meta:
+		verbose_name = 'Person'
+		verbose_name_plural = 'People'
+
+
+class StandardPage(Page):
+	"""
+	A generic content page. On this demo site we use it for an about page but
+	it could be used for any page content that only needs a title,
+	image, introduction, and body field
+	"""
+
+	introduction = models.TextField(
+			help_text='Text to describe the page',
+			blank=True,
+		)
+	image = models.ForeignKey(
+			'wagtailimages.Image',
+			null=True,
+			blank=True,
+			on_delete=models.SET_NULL,
+			related_name='+',
+			help_text='Landscape mode only; horizontal width between 1000px and 3000px'
+		)
+	body = StreamField(
+		BaseStreamBlock(), verbose_name="Page body", blank=True
+	)
+
+	content_panels = Page.content_panels + [
+		FieldPanel('introduction', classname="full"),
+		StreamFieldPanel('body'),
+		ImageChooserPanel('image'),
+	]
+
+
 
 class HomePage(Page):
+    """
+	The Home Page. This looks slightly more complicated then it is. You can see
+	if you visit your site and edit the homepage that it is split between a:
+		- Hero area
+		- Body area
+		- A promotional area
+		- Moveable feature site sections
+    """
+
+    # Hero section of HomePage
+    logo_image = models.ForeignKey(
+    		'wagtailimages.Image',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		help_text='Homepage logo',
+    	)
+    image = models.ForeignKey(
+    		'wagtailimages.Image',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		help_text='Homepage image',
+    	)
+    hero_text = models.CharField(
+    		max_length=255,
+    		help_text='Write an introduction for the business',
+    	)
+    hero_cta = models.CharField(
+    		verbose_name='Hero CTA',
+    		max_length=255,
+    		help_text='Text to display on Call to Action',
+    	)
+    hero_cta_link = models.ForeignKey(
+    		'wagtailcore.Page',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		verbose_name='Hero CTA link',
+    		help_text='Choose a page to link to for the Call to Action',
+    	)
+    phone_regex = RegexValidator(regex=r'(\(\d\d\d\)) (\d\d\d-\d\d\d\d)', message="Phone must be entered in the format (555) 567-8555")
+    phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True)
+    email = models.EmailField(max_length=100)
+
+    # Body section of the HomePage
     body = StreamField(
-    	BaseStreamBlock(), verbose_name="Page body", blank=True
+    	BaseStreamBlock(), verbose_name="Home content block", blank=True
     )
 
+    promo_image = models.ForeignKey(
+    		'wagtailimages.Image',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		help_text='Promo image',
+    	)
+    promo_title = models.CharField(
+    		null=True,
+    		blank=True,
+    		max_length=255,
+    		help_text='Title to display above the promo copy',
+    	)
+    promo_text = RichTextField(
+    		null=True,
+    		blank=True,
+    		help_text='Write some promotional copy',
+    	)
+
+    # Featured sections on the HomePage
+    # You will see on templates/home/home_page.html that these are treated
+    # in different ways, and displayed in different areas of the page.
+    # Each list their children items that we access via the children function
+    # that we define on the individual Page models e.g. BlogIndexPage
+    featured_section_1_title = models.CharField(
+    		null=True,
+    		blank=True,
+    		max_length=255,
+    		help_text='Title to display above the promo copy',
+    	)
+    featured_section_1 = models.ForeignKey(
+    		'wagtailcore.Page',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		help_text='First featured section for the homepage. Will display up to '
+    		'three child names.',
+    		verbose_name='Featured section 1',
+    	)
+    featured_section_2_title = models.CharField(
+    		null=True,
+    		blank=True,
+    		max_length=255,
+    		help_text='Title to display above the promo copy',
+    	)
+    featured_section_2 = models.ForeignKey(
+    		'wagtailcore.Page',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		help_text='First featured section for the homepage. Will display up to '
+    		'three child names.',
+    		verbose_name='Featured section 1',
+    	)
+    featured_section_3_title = models.CharField(
+    		null=True,
+    		blank=True,
+    		max_length=255,
+    		help_text='Title to display above the promo copy',
+    	)
+    featured_section_3 = models.ForeignKey(
+    		'wagtailcore.Page',
+    		null=True,
+    		blank=True,
+    		on_delete=models.SET_NULL,
+    		related_name='+',
+    		help_text='First featured section for the homepage. Will display up to '
+    		'three child names.',
+    		verbose_name='Featured section 1',
+    	)
+
     content_panels = Page.content_panels + [
+    	MultiFieldPanel([
+    		ImageChooserPanel('image'),
+    		ImageChooserPanel('logo_image'),
+    		FieldPanel('hero_text', classname="full"),
+    		FieldPanel('phone_number'),
+    		FieldPanel('email'),
+    		MultiFieldPanel([
+    			FieldPanel('hero_cta'),
+    			PageChooserPanel('hero_cta_link'),
+    			]),
+    		], heading="Hero section"),
+    	MultiFieldPanel([
+    		ImageChooserPanel('promo_image'),
+    		FieldPanel('promo_title'),
+    		FieldPanel('promo_text'),
+    		], heading="Promo section"),
     	StreamFieldPanel('body'),
+		MultiFieldPanel([
+			MultiFieldPanel([
+				FieldPanel('featured_section_1_title'),
+				PageChooserPanel('featured_section_1'),
+			]),
+			MultiFieldPanel([
+				FieldPanel('featured_section_2_title'),
+				PageChooserPanel('featured_section_2'),
+			]),
+			MultiFieldPanel([
+				FieldPanel('featured_section_3_title'),
+				PageChooserPanel('featured_section_3'),
+			]),
+		], heading="Featured homepage section", classname="collapsible")
     ]
+
+    def phone_number_fixer(self):
+    	HomePage.objects.get("phone_number")
+
+    # def get_context(self, request):
+    # 	context = super(HomePage, self).get_context(request)
+    # 	sp_list = [1, 2, 3, 4]
+    # 	bp_list = [1, 2, 3, 4, 5, 6]
+    # 	pgp_list = [1, 2]
+    # 	sp_objs = ServicePage.objects.all()
+    # 	bp_objs = BlogPage.objects.all()
+    # 	pgp_objs = PhotoGalleryPage.objects.all()
+    # 	for sp in sp_objs:
+    # 		for value in sp_list:
+    # 			if value == sp.feature and sp not in sp_list:
+    # 				sp_list.insert(value-1, sp)
+    # 				sp_list.remove(value)
+    # 	for bp in bp_objs:
+    # 		for value in bp_list:
+    # 			if value == bp.feature and bp not in bp_list:
+    # 				bp_list.insert(value-1, bp)
+    # 				bp_list.remove(value)
+    # 	for pgp in pgp_objs:
+    # 		for value in pgp_list:
+    # 			if value == pgp.feature and pgp not in pgp_list:
+    # 				pgp_list.insert(value-1, pgp)
+    # 				pgp_list.remove(value)
+    # 	context['sp_features_list'] = sp_list
+    # 	context['bp_features_list'] = bp_list
+    # 	context['pgp_features_list'] = pgp_list
+    # 	return context
+
+    def __str__(self):
+    	return self.title
+
+
+def GalleryPage(Page):
+	"""
+	This is a page to list locations from the selected Collection. We use a Q
+	object to list any collection created even if they contain no items.
+	"""
+	introduction = models.TextField(
+			help_text='Text to describe the page.',
+			blank=True)
+	image = models.ForeignKey(
+			'wagtailimages.Image',
+			null=True,
+			blank=True,
+			on_delete=models.SET_NULL,
+			related_name='+',
+			help_text='Landscape mode only; horizontal width between 1000px and 3000px.'
+		)
+	body = StreamField(
+		BaseStreamBlock(), verbose_name="Page body", blank=True
+	)
+	collection = models.ForeignKey(
+			Collection,
+			limit_choices_to=~model.Q(name__in=['Root']),
+			null=True,
+			blank=True,
+			on_delete=models.SET_NULL,
+			help_text='Select the image collection for this gallery.'
+		)
+
+	content_panels = Page.content_panels + [
+		FieldPanel('introduction', classname="full"),
+		StreamFieldPanel('body'),
+		ImageChooserPanel('image'),
+		FieldPanel('collection'),
+	]
+
+	# Defining what content type can sit under the parent. Since it's a blank
+	# array no subpage can be added
+	subpage_types = []
+
+
+class FormField(AbstractFormField):
+	"""
+	Wagtailforms is a module to introduce simple forms on a wagtail site. It isn't
+	intended as a replacement to Django's form support but as a quick way to generate
+	without having to write code. We use it on the site for a contact form.
+	"""
+	page = ParentalKey('FormPage', related_name='form_fields', on_delete=models.CASCADE)
+
+
+class FormPage(AbstractEmailForm):
+	image = models.ForeignKey(
+		'wagtailimages.Image',
+		null=True,
+		blank=True,
+		on_delete=models.SET_NULL,
+		related_name='+',
+		)
+	body = StreamField(BaseStreamBlock(), null=True)
+	thank_you_text = RichTextField(blank=True)
+
+	# Note how we include the FormField object vie an InlinePanel using
+	# related_name value
+	content_panels = AbstractEmailForm.content_panels + [
+	ImageChooserPanel('image'),
+	StreamFieldPanel('body'),
+	InlinePanel('form_fields', label="Form fields"),
+	FieldPanel('thank_you_text', classname="full"),
+	MultiFieldPanel([
+		FieldRowPanel([
+			FieldPanel('from_address', classname="form-page"),
+			FieldPanel('to_address', classname="form-page"),
+			]),
+		FieldPanel('subject'),
+		], "Email"),
+	]
+
+
+class HomePageImageGallery(Orderable):
+	page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='gallery_images')
+	image = models.ForeignKey(
+			'wagtailimages.Image', on_delete=models.CASCADE, related_name='+'
+		)
+	caption = models.CharField(blank=True, max_length=250)
+	logo = models.BooleanField(default=False)
+
+	panels = [
+		ImageChooserPanel('image'),
+		FieldPanel('caption'),
+		FieldPanel('logo', widget=forms.RadioSelect),
+	]
