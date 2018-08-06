c45faae1577b7f3e677dbf8eb415b6ac6e94f422
Andrew K Telleria <andrewktelleria@gmail.com>
2018-08-06 12:44:50 -0700

    recreated eb env

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -2,7 +2,7 @@ from .base import *
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-ALLOWED_HOSTS = ['portfolio-env.qkwxmxpt4x.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['portfolio-env.xqnippjwjj.us-west-2.elasticbeanstalk.com']
 
 DEBUG = False
 
