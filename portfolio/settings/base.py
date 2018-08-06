976587b81677ecfb98f5598659ff253e6c4b70d2
Andrew K Telleria <andrewktelleria@gmail.com>
2018-08-06 11:17:45 -0700

    allowed hosts

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -179,3 +179,10 @@ WAGTAIL_SITE_NAME = "portfolio"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+
+ALLOWED_HOSTS = ['portfolio-env.qkwxmxpt4x.us-west-2.elasticbeanstalk.com']
+
+
+
+
