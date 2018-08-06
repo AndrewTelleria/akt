a51ab2f0d9fa22f0517963b1171f198a7862a82e
Andrew K Telleria <andrewktelleria@gmail.com>
2018-08-06 12:48:00 -0700

    static files

---
+++ collect_static/js/canvas2.js
@@ -0,0 +1,5 @@
+var cv = document.getElementById('canvas2');
+var c1 = cv.getContext('2d');
+
+var x = randomIntFromRange(0,100);
+console.log(x);
---
+++ collect_static/js/reveal.js
@@ -0,0 +1,136 @@
+window.sr = ScrollReveal();
+
+// Section 1 - Projects and Skills
+
+sr.reveal('.section-1-title', { 
+	duration: 1000,
+	distance: '200px',
+	origin: 'top',
+	scale: 1.3,
+});
+
+sr.reveal('.promo', { 
+	duration: 1000,
+	distance: '200px',
+	origin: 'left',
+	scale: 0.7,
+	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+});
+
+sr.reveal('.promo-title', { 
+	duration: 1000,
+	distance: '200px',
+	origin: 'right',
+	scale: 1.3
+});
+
+sr.reveal('.promo-text', { 
+	duration: 1000,
+	distance: '500px',
+	origin: 'right',
+	scale: 0.5
+});
+
+sr.reveal('.skill-bar', { 
+	duration: 1000,
+	distance: '600px',
+	origin: 'right',
+	scale: 0.5,
+});
+
+sr.reveal('.section-1-childpage', { 
+	duration: 1000,
+	distance: '200px',
+	origin: 'top',
+	scale: 0.7,
+	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+});
+
+sr.reveal('.section-1-childpage-title', { 
+	duration: 1500,
+	delay: 100,
+	distance: '200px',
+	origin: 'top',
+	scale: 0.7,
+	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+});
+
+sr.reveal('.section-1-childpage-p', { 
+	duration: 1500,
+	delay: 500,
+	distance: '200px',
+	origin: 'bottom',
+	scale: 0.7,
+	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+});
+
+// Section 2 - Blog
+
+sr.reveal('.section-2-title', { 
+	duration: 1000,
+	distance: '200px',
+	origin: 'right',
+	scale: 1.3,
+});
+
+sr.reveal('.section-2-childpage', { 
+	duration: 1000,
+	distance: '200px',
+	origin: 'top',
+	scale: 0.7,
+	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+	rotate: { x: 0, y: 90, z: 10 },
+});
+
+sr.reveal('.section-2-childpage-title', { 
+	duration: 1500,
+	delay: 100,
+	distance: '200px',
+	origin: 'left',
+	scale: 0.7,
+	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+});
+
+sr.reveal('.section-2-childpage-p', { 
+	duration: 1500,
+	delay: 500,
+	distance: '200px',
+	origin: 'right',
+	scale: 0.7,
+	easing: 'cubic-bezier(0.6, 0.2, 0.1, 1)',
+});
+
+// Section 3
+
+sr.reveal('.homepage-contact', {
+	duration: 1500,
+	distance: '300px',
+	origin: 'right',
+	scale: 1.5,
+	rotate: { x: 0, y: 90, z: 0 },
+});
+
+sr.reveal('.resume', {
+	duration: 1500,
+	distance: '300px',
+	origin: 'left',
+	scale: 1.5,
+	rotate: { x: 0, y: -90, z: 0 },
+});
+
+sr.reveal('.homepage-body', {
+	duration: 1500,
+	distance: '300px',
+	origin: 'bottom',
+	scale: 0.5,
+});
+
+
+
+
+
+
+
+
+
+
