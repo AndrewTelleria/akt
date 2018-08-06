91edc213323c246bd096ebd3b19be81bba996b73
Andrew K Telleria <andrewktelleria@gmail.com>
2018-08-06 12:11:38 -0700

    updated manage module to have production settings

--- portfolio/settings/dev.py
+++ portfolio/settings/dev.py
@@ -7,7 +7,7 @@ DEBUG = True
 SECRET_KEY = '!yo3k%%n@kjn*5=$*dw9@xw3_d0n-ou$_yw%6)-0%1104sfen4'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -2,7 +2,6 @@ from .base import *
 import os
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
 ALLOWED_HOSTS = ['portfolio-env.qkwxmxpt4x.us-west-2.elasticbeanstalk.com']
 
 DEBUG = False
