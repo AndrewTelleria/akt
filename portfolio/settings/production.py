2b27e2dcc30d699bbb5fbefe648155db67c36f96
Andrew K Telleria <andrewktelleria@gmail.com>
2018-08-07 10:30:13 -0700

    Enabled debuggin

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['portfolio-env.6sduwvv6gr.us-west-2.elasticbeanstalk.com']
 
-DEBUG = False
+DEBUG = True
 
 try:
     from .local import *
