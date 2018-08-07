d0170262113d4946ff13a7bd3f60cc258c1adeee
Andrew K Telleria <andrewktelleria@gmail.com>
2018-08-07 10:35:50 -0700

    Disabled debugging

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -5,7 +5,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 ALLOWED_HOSTS = ['portfolio-env.6sduwvv6gr.us-west-2.elasticbeanstalk.com']
 
-DEBUG = True
+DEBUG = False
 
 try:
     from .local import *
