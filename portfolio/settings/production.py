501cada102028f42d9607b95ddda126b261e0819
Andrew K Telleria <andrewktelleria@gmail.com>
2018-08-10 11:39:59 -0700

    getting ready for deployment

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -1,7 +1,7 @@
 from .base import *
 import os
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 
 ALLOWED_HOSTS = ['portfolio-env.pam4nmw3wm.us-west-2.elasticbeanstalk.com']
 
@@ -11,3 +11,14 @@ try:
     from .local import *
 except ImportError:
     pass
+
+
+
+
+
+[program:akt]
+command = /home/ubuntu/gunicorn_start.bash
+user = ubuntu
+stdout_logfile = /home/ubuntu/logs/gunicorn_supervisor.log
+redirect_stderr = true
+environment=LANG=en_US.UTF-8,LC_ALL=en_US.UTF-8
